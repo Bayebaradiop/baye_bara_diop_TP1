7b3862bde6715a7e75b6379bed651a4ce1b420a3
Bayebaradiop <bayebara2000@gmail.com>
2026-04-23 16:12:33 +0000

    test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import product.Product;
+import valueobject.Money;
 import valueobject.SKU;
+
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
 
-        SKU s1 = new SKU("TEC-12345");
-        System.out.println(s1);
+        Product p = new Product(
+                new SKU("TEC-12345"),
+                "Laptop HP",
+                new Money(new BigDecimal("500000"), "FCFA")
+        );
 
+        System.out.println(p);
+
+        p.applyDiscount(new BigDecimal("10"));
+
+        System.out.println("Après réduction:");
+        System.out.println(p);
     }
 }
--- src/product/Product.java
+++ src/product/Product.java
@@ -1,4 +1,80 @@
 package product;
 
-public class Produit {
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import valueobject.Money;
+import valueobject.SKU;
+
+public class Product {
+
+    private final UUID id;
+    private final SKU sku;
+    private final String name;
+    private Money price;
+
+    public Product(SKU sku, String name, Money price) {
+
+        if (sku == null) {
+            throw new IllegalArgumentException("SKU obligatoire");
+        }
+
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Nom obligatoire");
+        }
+
+        if (price == null) {
+            throw new IllegalArgumentException("Prix obligatoire");
+        }
+
+        this.id = UUID.randomUUID();
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void applyDiscount(BigDecimal percentage) {
+
+        if (percentage == null ||
+                percentage.compareTo(BigDecimal.valueOf(0.1)) < 0 ||
+                percentage.compareTo(BigDecimal.valueOf(100)) > 0) {
+            throw new IllegalArgumentException("Pourcentage invalide");
+        }
+
+        BigDecimal reduction =
+                price.amount()
+                        .multiply(percentage)
+                        .divide(BigDecimal.valueOf(100));
+
+        BigDecimal newPrice =
+                price.amount().subtract(reduction);
+
+        this.price = new Money(newPrice, price.currency());
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public SKU getSku() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", sku=" + sku.value() +
+                ", name='" + name + '\'' +
+                ", price=" + price.amount() + " " + price.currency() +
+                '}';
+    }
 }
